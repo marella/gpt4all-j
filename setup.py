a5541a13393aac32d9cd05f6160d8d0c4e305d1a
Ravindra Marella <mv.ravindra007@gmail.com>
2023-04-23 23:49:02 +0530

    0.2.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ name = 'gpt4all-j'
 
 setup(
     name=name,
-    version='0.2.0',
+    version='0.2.1',
     description='Python bindings for the C++ port of GPT4All-J model.',
     long_description=long_description,
     long_description_content_type='text/markdown',
