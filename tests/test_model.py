de87babe5afa6b62cb26e8ffd08ff35f494f2a72
Ravindra Marella <mv.ravindra007@gmail.com>
2023-04-29 00:45:45 +0530

    Update tests

--- tests/test_model.py
+++ tests/test_model.py
@@ -6,4 +6,4 @@ class TestModel():
     def test_generate(self, model, instructions):
         model = Model(model, instructions=instructions)
         response = model.generate('AI is going to', seed=10, n_predict=3)
-        assert response == " be the new"
+        assert response == " be the future"
